20ea9b358981e9b4b74b5d7bb8482878210b3a9e
brianbassey37 <brianbassey37@gmail.com>
2023-02-16 15:16:23 -0800

    updated readme

--- models/user.py
+++ models/user.py
@@ -6,6 +6,7 @@ from os import getenv
 import sqlalchemy
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+import hashlib
 
 
 class User(BaseModel, Base):
@@ -24,6 +25,13 @@ class User(BaseModel, Base):
         first_name = ""
         last_name = ""
 
+    def __setattr__(self, name, value):
+        """custom password to hashed version"""
+        if name == 'password' and type(value) == str:
+            value = hashlib.md5(value.encode('utf-8')).hexdigest()
+        super.__setattr__(self, name, value)
+
     def __init__(self, *args, **kwargs):
         """initializes user"""
         super().__init__(*args, **kwargs)
+
